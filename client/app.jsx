d572a405bdca702fce45e6b28e8940228464b22f
Carling Sugarman <csugarman@hmc.edu>
2016-09-08 12:28:52 -0700

    [fix] - cleans up loading page

--- client/app.jsx
+++ client/app.jsx
@@ -68,10 +68,6 @@ componentWillMount() {
       return (
         <div>
           <NavBar />
-          <div className="container">
-            <h1>Detailed Search</h1>
-            <SearchBar infoStore={this.infoStore}/>
-           </div>
           <img src={'../car.gif'} className="spinner"/>
         </div>
       )
