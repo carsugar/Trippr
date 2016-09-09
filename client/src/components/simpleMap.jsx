069f601c0806ddd919ffb074b554aac7ca75ebd0
= <Jmitchell1991@gmail.com>
2016-09-08 18:29:53 -0700

    [client] - creates markers on googleMap

--- client/src/components/simpleMap.jsx
+++ client/src/components/simpleMap.jsx
@@ -1,25 +1,56 @@
-import React from "react";
+import React, {Component} from 'react';
 import {
   GoogleMapLoader,
   GoogleMap,
   Marker,
 } from "react-google-maps";
 
-const SimpleMap = (props) => {
-	console.log('props inside simpleMap****** are: ',props)
- 
 
 
+export default class SimpleMap extends Component{
+   constructor(props) {
+    super(props);
+  }
+  
 
 
+  shouldComponentUpdate(nextProps, nextState) {
+  console.log('nextProps',nextProps.centerPoints)
+  console.log('this.props.centerPoints',this.props.centerPoints)
 
+  return nextProps.centerPoints !== this.props.centerPoints;
+  }
+
+  //   if (this.props.centerPoints) {
+  // console.log('props centerpoints lat', this.props.centerPoints.avgLat)
+  // }
+
+  render() {
+    if (this.props.results.start_address) { 
+    var startObj = {
+      defaultAnimation: 2,
+      key: this.props.results.start_address,
+      position: {
+        lat: this.props.results.start_location.lat,
+        lng: this.props.results.start_location.lng
+      }
+    }
+    var endObj = {
+      defaultAnimation: 2,
+      key: this.props.results.end_address,
+      position: {
+        lat: this.props.results.end_location.lat,
+        lng: this.props.results.end_location.lng
+      }
+    }
+  }
 
 	return (
   <section style={{ height: `100%` }}>
     <GoogleMapLoader
       containerElement={
         <div
-          {...props.containerElementProps}
+          {...this.props.containerElementProps}
           style={{
             height: `100%`  }}
         />
@@ -28,20 +59,25 @@ const SimpleMap = (props) => {
         <GoogleMap
           ref={(map) => console.log(map)}
           defaultZoom={3}
-          defaultCenter={{ lat: -1.4 , lng: -1.4}}
-          onClick={props.onMapClick}
+          center={{ lat:  (this.props.centerPoints) ? this.props.centerPoints.avgLat: -31, 
+                           lng: (this.props.centerPoints) ? this.props.centerPoints.avgLng: 22
+                         }}
+          onClick={this.props.onMapClick}
         >
-          {props.markers.map((marker, index) => (
+          
             <Marker
-              {...marker}
-              onRightclick={() => props.onMarkerRightclick(index)}
+              {...startObj}
+              onRightclick={() => this.props.onMarkerRightclick(index)}
             />
-          ))}
+            <Marker
+              {...endObj}
+              onRightclick={() => this.props.onMarkerRightclick(index)}
+            />
         </GoogleMap>
       }
     />
   </section>
   )
+  }
 }
 
-export default SimpleMap;
